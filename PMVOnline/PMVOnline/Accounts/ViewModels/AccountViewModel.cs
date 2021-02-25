c2f2fb35dac11d345aee2b82d3f61b0da38afe22
edutalkapp <binh@edutalkapp.com>
2021-02-25 21:29:03 +0700

    binding for account page

---
+++ PMVOnline/PMVOnline/Accounts/ViewModels/AccountViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PMVOnline.Common.Bases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMVOnline.Accounts.ViewModels
+{
+    public class AccountViewModel : ViewModelBase
+    {
+    }
+}
---
+++ PMVOnline/PMVOnline/Common/Controls/LabelIconAction.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PMVOnline.Common.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabelIconAction : Frame
+    {
+        public static BindableProperty CommandProperty = BindableProperty.Create(
+                nameof(Command),
+                typeof(ICommand),
+                typeof(LabelIconAction),
+                default(ICommand),
+                BindingMode.TwoWay
+                );
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public static BindableProperty TextProperty = BindableProperty.Create(
+                nameof(Text),
+                typeof(string),
+                typeof(LabelIconAction),
+                default(string),
+                BindingMode.OneWay
+                );
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static BindableProperty IconProperty = BindableProperty.Create(
+                nameof(Icon),
+                typeof(string),
+                typeof(LabelIconAction),
+                default(string),
+                BindingMode.OneWay
+                );
+
+        public string Icon
+        {
+            get => (string)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        Color _color { get; set; }
+        public Color Color
+        {
+            get => _color;
+            set
+            {
+                _color = value;
+                bgIcon.BackgroundColor = value;
+                lblAction.TextColor = value;
+            }
+        }
+
+        public string IconAction
+        {
+            get => lblAction.Text;
+            set
+            {
+                lblAction.Text = value;
+                lblAction.IsVisible = !string.IsNullOrEmpty(value);
+            }
+        }
+
+        public LabelIconAction()
+        {
+            InitializeComponent();
+        }
+    }
+}
