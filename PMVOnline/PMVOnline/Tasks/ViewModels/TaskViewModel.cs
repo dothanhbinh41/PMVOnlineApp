7e229089f1377673435b05805c149c06de24ac90
edutalkapp <binh@edutalkapp.com>
2021-02-25 20:56:14 +0700

    Binding Tasks

--- PMVOnline/PMVOnline/Tasks/ViewModels/TaskViewModel.cs
+++ PMVOnline/PMVOnline/Tasks/ViewModels/TaskViewModel.cs
@@ -9,13 +9,13 @@ namespace PMVOnline.Tasks.ViewModels
 {
     public class TaskViewModel : ViewModelBase
     {
-        public List<HotTaskModel> Tasks { get; set; }
+        public List<TaskModel> Tasks { get; set; }
         public TaskViewModel()
         {
-            Tasks = new List<HotTaskModel> {
-                new HotTaskModel{ Assignee = "Do THanh Binh", Action = "Phe Duyet", DueDate = DateTime.Now.AddDays(2), Id = 123, Priority =  TaskPriority.High},
-                new HotTaskModel{ Assignee = "Do THanh Binh", Action = "Phe Duyet", DueDate = DateTime.Now.AddDays(2), Id = 123, Priority =  TaskPriority.Normal},
-                new HotTaskModel{ Assignee = "Do THanh Binh", Action = "Phe Duyet", DueDate = DateTime.Now.AddDays(2), Id = 123, Priority =  TaskPriority.Highest},
+            Tasks = new List<TaskModel> {
+                new TaskModel{ Assignee = "Do THanh Binh", Action = "Phe Duyet", DueDate = DateTime.Now.AddDays(2), Id = 123, Priority =  TaskPriority.High},
+                new TaskModel{ Assignee = "Do THanh Binh", Action = "Phe Duyet", DueDate = DateTime.Now.AddDays(2), Id = 123, Priority =  TaskPriority.Normal},
+                new TaskModel{ Assignee = "Do THanh Binh", Action = "Phe Duyet", DueDate = DateTime.Now.AddDays(2), Id = 123, Priority =  TaskPriority.Highest},
             };
         }
     }
