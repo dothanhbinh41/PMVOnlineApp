9ca28a570c04da34e214e277cb8483f2e2053e42
edutalkapp <binh@edutalkapp.com>
2021-03-20 22:05:22 +0700

    change api base

--- PMVOnline/PMVOnline.Api/ApiBase.cs
+++ PMVOnline/PMVOnline.Api/ApiBase.cs
@@ -4,7 +4,7 @@ namespace PMVOnline.Api
     public class ApiBase
     {
         public const string BearerScheme = "Bearer"; 
-        public const string ServerApi = "http://83f50a4e6508.ngrok.io"; 
+        public const string ServerApi = "https://pmvonline.azurewebsites.net"; 
         public const string ClientId = "PMVOnline_App"; 
         public const string ClientSecret = "1q2w3e*"; 
         public const string ClientScope = "offline_access PMVOnline"; 
